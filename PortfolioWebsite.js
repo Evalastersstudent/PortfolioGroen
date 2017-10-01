a316f496cb0e19022bfc41b8ac2fbfba52d53824
EVA LASTERS <EVA.LASTERS@STUDENT.AP.BE>
2017-10-02 01:05:43 +0200

    Portfolio groen

---
+++ PortfolioWebsite.js
@@ -0,0 +1,192 @@
+function main() {
+			var knop = document.getElementById("knop");
+			knop.onclick = function() {
+						knop.style.display="none";
+						var main = document.getElementById("main");
+						main.style.display="block";
+			}
+
+			var bambi2 = document.getElementById("bambi²");
+			bambi2.onclick = function() {
+						var bambi = document.getElementById("bambi");
+						if (bambi.className === "onzichtbaar") {
+									bambi.className = "zichtbaar";
+									abstract.className = "onzichtbaar";
+									creativebeat.className = "onzichtbaar";
+									eames.className = "onzichtbaar";
+									hairequipe.className = "onzichtbaar";
+						} else {
+									bambi.className = "onzichtbaar";
+									abstract.className = "onzichtbaar";
+									creativebeat.className = "onzichtbaar";
+									eames.className = "onzichtbaar";
+									hairequipe.className = "onzichtbaar";
+						}
+			}
+
+			var abstract2 = document.getElementById("abstract²");
+			abstract2.onclick = function() {
+						var abstract = document.getElementById("abstract");
+						if (abstract.className === "onzichtbaar") {
+									abstract.className = "zichtbaar";
+									bambi.className = "onzichtbaar";
+									creativebeat.className = "onzichtbaar";
+									eames.className = "onzichtbaar";
+									hairequipe.className = "onzichtbaar";
+						} else {
+									bambi.className = "onzichtbaar";
+									abstract.className = "onzichtbaar";
+									creativebeat.className = "onzichtbaar";
+									eames.className = "onzichtbaar";
+									hairequipe.className = "onzichtbaar";
+						}
+			}
+
+			var creativebeat2 = document.getElementById("creativebeat²");
+			creativebeat2.onclick = function() {
+						var creativebeat = document.getElementById("creativebeat");
+						if (creativebeat.className === "onzichtbaar") {
+									creativebeat.className = "zichtbaar";
+									bambi.className = "onzichtbaar";
+									abstract.className = "onzichtbaar";
+									eames.className = "onzichtbaar";
+									hairequipe.className = "onzichtbaar";
+						} else {
+									bambi.className = "onzichtbaar";
+									abstract.className = "onzichtbaar";
+									creativebeat.className = "onzichtbaar";
+									eames.className = "onzichtbaar";
+									hairequipe.className = "onzichtbaar";
+						}
+			}
+
+			var eames2 = document.getElementById("eames²");
+			eames2.onclick = function() {
+						var eames = document.getElementById("eames");
+						if (eames.className === "onzichtbaar") {
+									eames.className = "zichtbaar";
+									bambi.className = "onzichtbaar";
+									abstract.className = "onzichtbaar";
+									creativebeat.className = "onzichtbaar";
+									hairequipe.className = "onzichtbaar";
+						} else {
+									bambi.className = "onzichtbaar";
+									abstract.className = "onzichtbaar";
+									creativebeat.className = "onzichtbaar";
+									eames.className = "onzichtbaar";
+									hairequipe.className = "onzichtbaar";
+						}
+			}
+
+			var hairequipe2 = document.getElementById("hairequipe²");
+			hairequipe2.onclick = function() {
+						var hairequipe = document.getElementById("hairequipe");
+						if (hairequipe.className === "onzichtbaar") {
+									hairequipe.className = "zichtbaar";
+									bambi.className = "onzichtbaar";
+									abstract.className = "onzichtbaar";
+									creativebeat.className = "onzichtbaar";
+									eames.className = "onzichtbaar";
+						} else {
+									bambi.className = "onzichtbaar";
+									abstract.className = "onzichtbaar";
+									creativebeat.className = "onzichtbaar";
+									eames.className = "onzichtbaar";
+									hairequipe.className = "onzichtbaar";
+						}
+			}
+
+			var transparantie12 = document.getElementById("transparantie1²");
+			transparantie12.onclick = function() {
+						var transparantie1 = document.getElementById("transparantie1");
+						if (transparantie1.className === "onzichtbaar") {
+									transparantie1.className = "zichtbaar";
+									logos.className = "onzichtbaar";
+									transparantie2.className = "onzichtbaar";
+									johnny.className = "onzichtbaar";
+									transparantie3.className = "onzichtbaar";
+						} else {
+									transparantie1.className = "onzichtbaar";
+									logos.className = "onzichtbaar";
+									transparantie2.className = "onzichtbaar";
+									johnny.className = "onzichtbaar";
+									transparantie3.className = "onzichtbaar";
+						}
+			}
+
+			var logos2 = document.getElementById("logos²");
+			logos2.onclick = function() {
+						var logos = document.getElementById("logos");
+						if (logos.className === "onzichtbaar") {
+									logos.className = "zichtbaar";
+									transparantie1.className = "onzichtbaar";
+									transparantie2.className = "onzichtbaar";
+									johnny.className = "onzichtbaar";
+									transparantie3.className = "onzichtbaar";
+						} else {
+									transparantie1.className = "onzichtbaar";
+									logos.className = "onzichtbaar";
+									transparantie2.className = "onzichtbaar";
+									johnny.className = "onzichtbaar";
+									transparantie3.className = "onzichtbaar";
+						}
+			}
+
+			var transparantie22 = document.getElementById("transparantie2²");
+			transparantie22.onclick = function() {
+						var transparantie2 = document.getElementById("transparantie2");
+						if (transparantie2.className === "onzichtbaar") {
+									transparantie2.className = "zichtbaar";
+									transparantie1.className = "onzichtbaar";
+									logos.className = "onzichtbaar";
+									johnny.className = "onzichtbaar";
+									transparantie3.className = "onzichtbaar";
+						} else {
+									transparantie1.className = "onzichtbaar";
+									logos.className = "onzichtbaar";
+									transparantie2.className = "onzichtbaar";
+									johnny.className = "onzichtbaar";
+									transparantie3.className = "onzichtbaar";
+						}
+			}
+
+			var johnny2 = document.getElementById("johnny²");
+			johnny2.onclick = function() {
+						var johnny = document.getElementById("johnny");
+						if (johnny.className === "onzichtbaar") {
+									johnny.className = "zichtbaar";
+									transparantie1.className = "onzichtbaar";
+									logos.className = "onzichtbaar";
+									transparantie2.className = "onzichtbaar";
+									transparantie3.className = "onzichtbaar";
+						} else {
+									transparantie1.className = "onzichtbaar";
+									logos.className = "onzichtbaar";
+									transparantie2.className = "onzichtbaar";
+									johnny.className = "onzichtbaar";
+									transparantie3.className = "onzichtbaar";
+						}
+			}
+			
+			var transparantie32 = document.getElementById("transparantie3²");
+			transparantie32.onclick = function() {
+						var transparantie3 = document.getElementById("transparantie3");
+						if (transparantie3.className === "onzichtbaar") {
+									transparantie3.className = "zichtbaar";
+									transparantie1.className = "onzichtbaar";
+									logos.className = "onzichtbaar";
+									transparantie2.className = "onzichtbaar";
+									johnny.className = "onzichtbaar";
+						} else {
+									transparantie1.className = "onzichtbaar";
+									logos.className = "onzichtbaar";
+									transparantie2.className = "onzichtbaar";
+									johnny.className = "onzichtbaar";
+									transparantie3.className = "onzichtbaar";
+						}
+			}
+}
+
+window.onload = function() {
+			main();
+}
